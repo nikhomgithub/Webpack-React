8a1ffc291967341ae2ca27a0c9e27cf6ed2552fc
nikhomgithub <mr_nikom@yahoo.com>
2019-08-13 20:14:15 +0700

    first commit

---
+++ webpack-react-css/components/SubApp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './SubApp.css'
+export default class SupApp extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This is Sub App</h2>
+            </div>
+        )
+    }
+}
